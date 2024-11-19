5730b0237e902003fce351f224108e3e7b0842db
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-11-19 22:47:35 +0000

    udate ideal_dag based on mwaa local test
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- contrib/ideal_dag.py
+++ contrib/ideal_dag.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 
-from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
+from airflow import DAG, AirflowException
+from airflow.decorators import task
+from airflow.models import Variable
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import \
+    KubernetesPodOperator
 from kubernetes.client import models as k8s_models
+
+# TODO - Review eval() function, should be bool() instead, but bool("False") returns True - go figure...
+maintenance_window_active = eval(
+    Variable.get("MAINTENANCE_WINDOW_ACTIVE", default_var="False")
+)
 
 default_args = {
     "owner": "analytical-platform@justice.gov.uk",
@@ -20,12 +26,30 @@ dag = DAG(
     default_args=default_args,
 )
 
+
+@task(
+    task_id="maintenance_window_check",
+    default_args={"owner": "analytical-platform@justice.gov.uk"},
+)
+def maintenance_window_check():
+    print(maintenance_window_active)
+    if maintenance_window_active:
+        raise AirflowException(
+            "There is an active maintenance window. Please try again later."
+        )
+    else:
+        pass
+
+
+maintenance_check = maintenance_window_check()
+
 task = KubernetesPodOperator(
     dag=dag,
     task_id="analytical-platform-example",
     namespace="airflow",
     name="analytical-platform-example",
     image="ghcr.io/ministryofjustice/analytical-platform-airflow-python-base:1.1.0",
+    cmds=["sleep", "60"],  # EXPERIMENTAL
     annotations={"karpenter.sh/do-not-disrupt": "true"},
     labels={
         "airflow.compute.analytical-platform.service.justice.gov.uk/environment": "development",
@@ -62,17 +86,16 @@ task = KubernetesPodOperator(
     security_context={
         "runAsNonRoot": True,
         "runAsUser": 1000,
-        "allowPrivilegeEscalation": False,
+        # "allowPrivilegeEscalation": False,
         "privileged": False,
     },
-    service_account_name="analytical-platform-example",
+    # service_account_name="analytical-platform-example",
     in_cluster=False,
     get_logs=True,
     startup_timeout_seconds=600,
     is_delete_operator_pod=True,
     # This doesn't work when running locally
-    # cluster_context="analytical-platform-compute-development",
-    # config_file="/usr/local/airflow/dags/.kube/config",
+    config_file="/usr/local/airflow/dags/.kube/config",
 )
 
-task.dry_run()
+task << maintenance_check
