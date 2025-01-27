8a331a26a29e76932c82183b5fa142ca949d1350
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-01-27 14:59:19 +0000

    Add zizmor Run zizmor Add Terraform workflow
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Install dependencies
+pip install --upgrade pip
 pip install --requirement requirements-dev.txt
---
+++ terraform/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket               = "mojap-common-production-tfstate"
+    region               = "eu-west-2"
+    encrypt              = true
+    kms_key_id           = "arn:aws:kms:eu-west-2:509399598587:key/ffd33710-74c9-46e0-9025-4de4571a1cae"
+    use_lockfile         = true
+    workspace_key_prefix = "analytical-platform-airflow"
+    key                  = "terraform.tfstate"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+  required_version = "~> 1.10"
+}
