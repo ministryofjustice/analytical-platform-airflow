b0078ff1eaeccb369d5a170975b1966153d12108
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-01-27 19:22:38 +0000

    fixes (hopefully)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 module "airflow" {
   for_each = {
     for f in fileset(path.module, "../environments/${terraform.workspace}/**/workflow.yml") :
-    join("/", slice(split("/", dirname(f)), 3, 6)) => f
+    join("/", slice(split("/", dirname(f)), 2, 4)) => f
   }
 
   source = "./modules/airflow"
 
-  project       = format("%s", split("/", each.key)[1])
-  workflow      = format("%s", split("/", each.key)[2])
-  environment   = format("%s", split("/", each.key)[0])
-  configuration = yamldecode(file("../environments/${each.key}/workflow.yml"))
+  project       = format("%s", split("/", each.key)[0])
+  workflow      = format("%s", split("/", each.key)[1])
+  environment   = terraform.workspace
+  configuration = yamldecode(file("../environments/${terraform.workspace}/${each.key}/workflow.yml"))
 }
