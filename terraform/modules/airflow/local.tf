0347f5e33553836f9332aa1e0d89d042e4cfcd02
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-01-28 16:37:19 +0000

    try tolist again
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/modules/airflow/local.tf
+++ terraform/modules/airflow/local.tf
@@ -3,5 +3,5 @@ locals {
   iam_external_role   = try(local.iam_configuration.external_role, null)
   iam_bedrock_enabled = try(local.iam_configuration.bedrock, false)
 
-  secrets_configuration = try(var.configuration.secrets, {})
+  secrets_configuration = try(var.configuration.secrets, [])
 }
