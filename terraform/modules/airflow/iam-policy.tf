79c15971eb330782813475e48db2a052b7ed2aff
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-13 17:41:31 +0000

    🛂 IAM builder parity (#34)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/modules/airflow/iam-policy.tf
+++ terraform/modules/airflow/iam-policy.tf
@@ -20,7 +20,147 @@ data "aws_iam_policy_document" "iam_policy" {
     resources = [
       "arn:aws:secretsmanager:eu-west-2:593291632749:secret:/airflow/${var.environment}/${var.project}/${var.workflow}/*",
       "arn:aws:secretsmanager:eu-west-1:593291632749:secret:/airflow/${var.environment}/${var.project}/${var.workflow}/*"
-    ] // TODO: do something clever with the output of module.secrets_manager
+    ]
+  }
+
+  /* Athena - Read Only */
+  dynamic "statement" {
+    for_each = local.iam_athena == "read" || local.iam_athena == "write" ? [1] : []
+    content {
+      sid    = "AthenaReadOnlyS3BucketActions"
+      effect = "Allow"
+      actions = [
+        "s3:GetBucketLocation",
+        "s3:ListAllMyBuckets"
+      ]
+      resources = ["*"]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_athena == "read" || local.iam_athena == "write" ? [1] : []
+    content {
+      sid     = "AthenaReadOnlyS3ListBuckets"
+      effect  = "Allow"
+      actions = ["s3:ListBucket"]
+      resources = [
+        "arn:aws:s3:::alpha-athena-query-dump",
+        "arn:aws:s3:::moj-analytics-lookup-tables",
+        "arn:aws:s3:::mojap-athena-query-dump"
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_athena == "read" || local.iam_athena == "write" ? [1] : []
+    content {
+      sid       = "AthenaReadOnlyS3GetObjects"
+      effect    = "Allow"
+      actions   = ["s3:GetObject"]
+      resources = ["arn:aws:s3:::moj-analytics-lookup-tables/*"]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_athena == "read" || local.iam_athena == "write" ? [1] : []
+    content {
+      sid    = "AthenaReadOnlyS3GetPutObjects"
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+        "s3:PutObject"
+      ]
+      resources = ["arn:aws:s3:::aws-athena-query-results-*"]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_athena == "read" || local.iam_athena == "write" ? [1] : []
+    content {
+      sid    = "AthenaReadOnlyS3DeleteGetPutObjects"
+      effect = "Allow"
+      actions = [
+        "s3:DeleteObject",
+        "s3:GetObject",
+        "s3:PutObject"
+      ]
+      resources = [
+        "arn:aws:s3:::alpha-athena-query-dump/$${aws:userid}/*",
+        "arn:aws:s3:::mojap-athena-query-dump/$${aws:userid}/*"
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_athena == "read" || local.iam_athena == "write" ? [1] : []
+    content {
+      sid    = "AthenaReadOnlyAthenaGlueRead"
+      effect = "Allow"
+      actions = [
+        "athena:BatchGetNamedQuery",
+        "athena:BatchGetQueryExecution",
+        "athena:CancelQueryExecution",
+        "athena:GetCatalogs",
+        "athena:GetExecutionEngine",
+        "athena:GetExecutionEngines",
+        "athena:GetNamedQuery",
+        "athena:GetNamespace",
+        "athena:GetNamespaces",
+        "athena:GetQueryExecution",
+        "athena:GetQueryResults",
+        "athena:GetQueryResultsStream",
+        "athena:GetTable",
+        "athena:GetTableMetadata",
+        "athena:GetTables",
+        "athena:GetWorkGroup",
+        "athena:ListNamedQueries",
+        "athena:ListQueryExecutions",
+        "athena:ListWorkGroups",
+        "athena:RunQuery",
+        "athena:StartQueryExecution",
+        "athena:StopQueryExecution",
+        "glue:BatchGetPartition",
+        "glue:GetCatalogImportStatus",
+        "glue:GetDatabase",
+        "glue:GetDatabases",
+        "glue:GetPartition",
+        "glue:GetPartitions",
+        "glue:GetTable",
+        "glue:GetTableVersions",
+        "glue:GetTables",
+        "glue:GetUserDefinedFunction",
+        "glue:GetUserDefinedFunctions"
+      ]
+      resources = ["*"]
+    }
+  }
+
+  /* Athena - Write */
+  dynamic "statement" {
+    for_each = local.iam_athena == "write" ? [1] : []
+    content {
+      sid    = "AthenaWrite"
+      effect = "Allow"
+      actions = [
+        "athena:DeleteNamedQuery",
+        "glue:BatchCreatePartition",
+        "glue:BatchDeletePartition",
+        "glue:BatchDeleteTable",
+        "glue:CreateDatabase",
+        "glue:CreatePartition",
+        "glue:CreateTable",
+        "glue:CreateUserDefinedFunction",
+        "glue:DeleteDatabase",
+        "glue:DeletePartition",
+        "glue:DeleteTable",
+        "glue:DeleteUserDefinedFunction",
+        "glue:UpdateDatabase",
+        "glue:UpdatePartition",
+        "glue:UpdateTable",
+        "glue:UpdateUserDefinedFunction"
+      ]
+      resources = ["*"]
+    }
   }
 
   /* Bedrock */
@@ -84,11 +224,97 @@ data "aws_iam_policy_document" "iam_policy" {
     }
   }
 
+  /* Glue */
+  dynamic "statement" {
+    for_each = local.iam_glue ? [1] : []
+    content {
+      sid    = "GlueActions"
+      effect = "Allow"
+      actions = [
+        "glue:BatchGetJobs",
+        "glue:BatchStopJobRun",
+        "glue:CreateJob",
+        "glue:DeleteJob",
+        "glue:GetJob",
+        "glue:GetJobBookmark",
+        "glue:GetJobRun",
+        "glue:GetJobRuns",
+        "glue:GetJobs",
+        "glue:ListJobs",
+        "glue:StartJobRun",
+        "glue:UpdateJob"
+      ]
+      resources = ["*"]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_glue ? [1] : []
+    content {
+      sid    = "GlueLogs"
+      effect = "Allow"
+      actions = [
+        "logs:DescribeLogStreams",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:GetLogEvents",
+        "logs:PutLogEvents"
+      ]
+      resources = ["*"]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_glue ? [1] : []
+    content {
+      sid    = "GlueMetrics"
+      effect = "Allow"
+      actions = [
+        "cloudwatch:GetMetricData",
+        "cloudwatch:ListDashboards",
+        "cloudwatch:PutMetricData"
+      ]
+      resources = ["*"]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_glue ? [1] : []
+    content {
+      sid    = "GlueS3"
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+        "s3:PutObject"
+      ]
+      resources = [
+        "arn:aws:s3:::aws-glue-*/*",
+        "arn:aws:s3:::*/*aws-glue-*/*",
+        "arn:aws:s3:::aws-glue-*"
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = local.iam_glue ? [1] : []
+    content {
+      sid       = "GluePassRole"
+      effect    = "Allow"
+      actions   = ["iam:PassRole"]
+      resources = ["arn:aws:iam::593291632749:role/airflow-${var.environment}-${var.project}-${var.workflow}"]
+      condition {
+        test     = "StringEquals"
+        variable = "iam:PassedToService"
+        values   = ["glue.amazonaws.com"]
+      }
+    }
+  }
+
   /* KMS */
   dynamic "statement" {
     for_each = length(local.iam_kms_keys) > 0 ? [1] : []
     content {
-      sid    = "KMSAccess"
+      sid    = "KMS"
       effect = "Allow"
       actions = [
         "kms:Decrypt",
@@ -99,6 +325,28 @@ data "aws_iam_policy_document" "iam_policy" {
       ]
       resources = [
         for item in local.iam_kms_keys : item
+      ]
+    }
+  }
+
+  /* S3 - Deny */
+  dynamic "statement" {
+    for_each = length(local.iam_s3_deny) > 0 ? [1] : []
+    content {
+      sid    = "S3Deny"
+      effect = "Deny"
+      actions = [
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion",
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+        "s3:GetObjectVersion",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:RestoreObject"
+      ]
+      resources = [
+        for item in local.iam_s3_deny : "arn:aws:s3:::${item}"
       ]
     }
   }
@@ -140,6 +388,25 @@ data "aws_iam_policy_document" "iam_policy" {
       ]
       resources = [
         for item in local.iam_s3_read_write : "arn:aws:s3:::${item}"
+      ]
+    }
+  }
+
+  /* S3 - Write Only */
+  dynamic "statement" {
+    for_each = length(local.iam_s3_write_only) > 0 ? [1] : []
+    content {
+      sid    = "S3WriteOnly"
+      effect = "Allow"
+      actions = [
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:RestoreObject"
+      ]
+      resources = [
+        for item in local.iam_s3_write_only : "arn:aws:s3:::${item}"
       ]
     }
   }
