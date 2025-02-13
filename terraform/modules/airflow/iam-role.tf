3c5ed739f087ee283deb9565e197fae5d0de234d
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-13 21:38:49 +0000

    Update IAM roles and permissions for Athena, Bedrock, and Glue
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/modules/airflow/iam-role.tf
+++ terraform/modules/airflow/iam-role.tf
@@ -31,10 +31,46 @@ resource "aws_iam_role_policy_attachment" "generated" {
 }
 
 resource "aws_iam_role_policy_attachment" "athena_read" {
-  count = length(local.iam_external_role) == 0 && local.iam_athena == "read" ? 1 : 0
+  count = length(local.iam_external_role) == 0 && (local.iam_athena == "read" || local.iam_athena == "write") ? 1 : 0
 
   provider = aws.analytical-platform-data-production-eu-west-2
 
   role       = module.iam_role[0].iam_role_name
   policy_arn = "arn:aws:iam::593291632749:policy/airflow-service/athena-read"
+}
+
+resource "aws_iam_role_policy_attachment" "athena_write" {
+  count = length(local.iam_external_role) == 0 && local.iam_athena == "write" ? 1 : 0
+
+  provider = aws.analytical-platform-data-production-eu-west-2
+
+  role       = module.iam_role[0].iam_role_name
+  policy_arn = "arn:aws:iam::593291632749:policy/airflow-service/athena-write"
+}
+
+resource "aws_iam_role_policy_attachment" "bedrock" {
+  count = length(local.iam_external_role) == 0 && local.iam_bedrock ? 1 : 0
+
+  provider = aws.analytical-platform-data-production-eu-west-2
+
+  role       = module.iam_role[0].iam_role_name
+  policy_arn = "arn:aws:iam::593291632749:policy/airflow-service/bedrock"
+}
+
+resource "aws_iam_role_policy_attachment" "glue" {
+  count = length(local.iam_external_role) == 0 && local.iam_glue ? 1 : 0
+
+  provider = aws.analytical-platform-data-production-eu-west-2
+
+  role       = module.iam_role[0].iam_role_name
+  policy_arn = "arn:aws:iam::593291632749:policy/airflow-service/glue"
+}
+
+resource "aws_iam_role_policy_attachment" "kms" {
+  count = length(local.iam_external_role) == 0 ? 1 : 0
+
+  provider = aws.analytical-platform-data-production-eu-west-2
+
+  role       = module.iam_role[0].iam_role_name
+  policy_arn = "arn:aws:iam::593291632749:policy/airflow-service/kms"
 }
