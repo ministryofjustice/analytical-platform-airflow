19474ce3d81b7ad8b8c399d9b9fe7bbdb77b56c8
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-13 21:26:18 +0000

    add provider
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/modules/airflow/iam-role.tf
+++ terraform/modules/airflow/iam-role.tf
@@ -24,12 +24,16 @@ module "iam_role" {
 resource "aws_iam_role_policy_attachment" "generated" {
   count = length(local.iam_external_role) == 0 ? 1 : 0
 
+  provider = aws.analytical-platform-data-production-eu-west-2
+
   role       = module.iam_role[0].iam_role_name
   policy_arn = module.iam_policy[0].arn
 }
 
 resource "aws_iam_role_policy_attachment" "athena_read" {
   count = length(local.iam_external_role) == 0 && local.iam_athena == "read" ? 1 : 0
+
+  provider = aws.analytical-platform-data-production-eu-west-2
 
   role       = module.iam_role[0].iam_role_name
   policy_arn = "arn:aws:iam::593291632749:policy/airflow-service/athena-read"
