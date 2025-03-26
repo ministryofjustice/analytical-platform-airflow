8c4d7cda3bca71a6121f20a8f84b5fe92cbfe81e
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-03-26 21:33:53 +0000

    Remove looping from work generation and validation Add steps to validation workflow
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/workflow_validation/main.py
+++ scripts/workflow_validation/main.py
@@ -1,26 +1,22 @@
 """Script to validate generated workflow can be loaded by dagfactory."""
-
-import glob
-import os
 import sys
 from pathlib import Path
 
 import dagfactory
 from dagfactory.exceptions import DagFactoryConfigException
 
-# Loop over generated workflows
-for root, dirs, files in os.walk("dist/dags"):
-    for file in glob.glob(os.path.join(root, "workflow.yml")):
-        print(f"Processing workflow: {file}")
-        file_path = Path(file).resolve()
-        try:
-            dag_factory = dagfactory.DagFactory(config_filepath=str(file_path))
-            dag = dag_factory.generate_dags(globals())
-        except DagFactoryConfigException as e:
-            print(f"Error loading DAG configuration: {str(e)}", file=sys.stderr)
-            sys.exit(1)
-        except Exception as e:
-            print(f"Unexpected error while processing DAG: {str(e)}", file=sys.stderr)
-            sys.exit(1)
-        else:
-            print(f"DAG loaded successfully: {file_path}")
+workflow_file = sys.argv[1]
+print(f"Processing workflow file: {workflow_file}")
+
+file_path = Path(workflow_file).resolve()
+try:
+    dag_factory = dagfactory.DagFactory(config_filepath=str(file_path))
+    dag = dag_factory.generate_dags(globals())
+except DagFactoryConfigException as e:
+    print(f"Error loading DAG configuration: {str(e)}", file=sys.stderr)
+    sys.exit(1)
+except Exception as e:
+    print(f"Unexpected error while processing DAG: {str(e)}", file=sys.stderr)
+    sys.exit(1)
+else:
+    print(f"DAG loaded successfully: {file_path}")
