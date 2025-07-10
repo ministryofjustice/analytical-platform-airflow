0cbafc4dfda9c06ca678fb6b065b4c34feff2b1a
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-07-10 10:43:18 +0100

    Update standard_operator.py

--- airflow/analytical_platform/standard_operator.py
+++ airflow/analytical_platform/standard_operator.py
@@ -11,7 +11,7 @@ from kubernetes.client import models as k8s_models
 
 def override_xcom_sidecar_defaults():
     """Override the default sidecar container for XCom in KubernetesPodOperator"""
-    PodDefaults.SIDECAR_CONTAINER.image = "ghcr.io/ministryofjustice/analytical-platform-airflow-xcom-sidecar:1.0.0-rc2@sha256:1e0adde6f97c66b64bbb213f43d83e0c54e45dcf5422c23628e3a157dc7172ad"
+    PodDefaults.SIDECAR_CONTAINER.image = "ghcr.io/ministryofjustice/analytical-platform-airflow-xcom-sidecar:1.0.0@sha256:83c145bccf9f112b7081759e37823b08d6d4b62cf4cb4573da5265cad1db0904"
     PodDefaults.SIDECAR_CONTAINER.security_context = k8s_models.V1SecurityContext(
         allow_privilege_escalation=False,
         privileged=False,
