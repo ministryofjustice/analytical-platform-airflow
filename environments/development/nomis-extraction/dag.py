dd161765fd14de2ad96eadc684ce23c38e52712f
Anthony Cody <anthony.cody@digital.justice.gov.uk>
2025-10-13 12:02:21 +0100

    add initialise_dag

--- environments/development/nomis-extraction/dag.py
+++ environments/development/nomis-extraction/dag.py
@@ -37,6 +37,43 @@ dag = DAG(
 
 tasks = dict()
 
+
+
+task_id = "initalise-dag"
+tasks[task_id] = KubernetesPodOperator(
+    dag=dag,
+    namespace="airflow",
+    image=IMAGE,
+    env_vars={
+        "PYTHON_SCRIPT_NAME": "initialise_dag.py",
+        "NOMIS_T62_FETCH_SIZE": DELTA_FETCH_SIZE,
+        "AWS_METADATA_SERVICE_TIMEOUT": "60",
+        "AWS_METADATA_SERVICE_NUM_ATTEMPTS": "5",
+        "BUILD_IMAGE": BUILD_IMAGE,
+        "DAG_ID": dag.dag_id,
+        "ENV": "PRODUCTION",
+    },
+    startup_timeout_seconds=600,
+    pool="nomis_tasks",
+    labels={"app": dag.dag_id},
+    name=task_id,
+    task_id=task_id,
+    get_logs=True,
+    in_cluster=False,
+    security_context={
+        "runAsNonRoot": True,
+        "allowPrivilegeEscalation": False,
+        "runAsUser": 1337,
+        "privileged": False,
+    },
+    is_delete_operator_pod=True,
+    cluster_context="analytical-platform-compute-production",
+    service_account_name=ROLE.replace("_", "-"),
+    config_file="/usr/local/airflow/dags/.kube/config",
+    tolerations=tolerations,
+    affinity=affinity,
+)
+
 # Trigger for downstream pipeline
 # dep_dag_id = "nomis.nomis_transform"
 
@@ -116,7 +153,7 @@ tasks[task_id] = KubernetesPodOperator(
 (
     tasks["nomis-delta-extract"]
     >> tasks["nomis-delta-extract-check"]
-#    >> tasks[f"trigger_for_{dep_dag_id}"]
+    >> tasks["initialise_dag"]
 )
 
 # Deletes
@@ -195,5 +232,5 @@ for i, L in PK_EXTRACTIONS.items():
     (
         tasks[f"nomis-pk-deletes-extract-{i}"]
         >> tasks[f"nomis-pk-deletes-extract-check-{i}"]
-        >> tasks[f"trigger_for_{dep_dag_id}"]
+        >> tasks["initialise_dag"]
     )
