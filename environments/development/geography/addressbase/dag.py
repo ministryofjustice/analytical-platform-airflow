434b7342460b08caea9f981f835f3b2589b31f19
Philip <112626966+p-sin@users.noreply.github.com>
2025-10-21 13:04:00 +0100

    Geog: Addressbase - move raw_to_curated to use multi processing (#1788)
    * Geog: Addressbase - move raw_to_curated to use multi processing
    
    * Geog: Addressbase - add missing _
    
    * Geog: remove whitespace

--- environments/development/geography/addressbase/dag.py
+++ environments/development/geography/addressbase/dag.py
@@ -66,16 +66,16 @@ tasks["to_land"] = AnalyticalPlatformStandardOperator(
     ],
 )
 
-tasks["raw_hist_to_curated"] = AnalyticalPlatformStandardOperator(
+tasks["create_curated_database"] = AnalyticalPlatformStandardOperator(
     dag=dag,
-    task_id="raw_hist_to_curated",
+    task_id="create_curated_database",
     name=f"{PROJECT}.{WORKFLOW}",
     compute_profile="general-spot-1vcpu-4gb",
     image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
     environment=f"{ENVIRONMENT}",
     project=f"{PROJECT}",
     workflow=f"{WORKFLOW}",
-    env_vars=update_env_vars(base_env_vars, {"STEP": "raw_hist_to_curated"})
+    env_vars=update_env_vars(base_env_vars, {"STEP": "create_curated_database"})
 )
 
 raw_tables = ["addressbasepremium"]
@@ -108,6 +108,19 @@ for table in raw_tables:
         env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw", "TOTAL_WORKERS": total_workers, "CLOSE": True, "TABLE": table})
     )
 
+    tasks[f"raw_hist_to_curated_init_{table}"] = AnalyticalPlatformStandardOperator(
+        dag=dag,
+        task_id=f"raw_hist_to_curated_init_{table}",
+        name=f"{PROJECT}.{WORKFLOW}",
+        compute_profile="general-spot-1vcpu-4gb",
+        image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
+        environment=f"{ENVIRONMENT}",
+        project=f"{PROJECT}",
+        workflow=f"{WORKFLOW}",
+        env_vars=update_env_vars(base_env_vars, {"STEP": "raw_hist_to_curated", "TOTAL_WORKERS": total_workers, "CLOSE": False, "TABLE": table})
+    )
+    tasks[f"land_to_raw_close_{table}"] >> tasks[f"raw_hist_to_curated_init_{table}"]
+
     for batch in range (total_workers):
         tasks[f"land_to_raw_{table}_{batch}"] = AnalyticalPlatformStandardOperator(
             dag=dag,
@@ -122,18 +135,32 @@ for table in raw_tables:
         )
         tasks[f"land_to_raw_hist_init_{table}"] >> tasks[f"land_to_raw_{table}_{batch}"]
         tasks[f"land_to_raw_{table}_{batch}"] >> tasks[f"land_to_raw_close_{table}"]
-        tasks[f"land_to_raw_close_{table}"] >> tasks[f"raw_hist_to_curated"]
 
 
-tasks["create_curated_database"] = AnalyticalPlatformStandardOperator(
-    dag=dag,
-    task_id="create_curated_database",
-    name=f"{PROJECT}.{WORKFLOW}",
-    compute_profile="general-spot-1vcpu-4gb",
-    image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
-    environment=f"{ENVIRONMENT}",
-    project=f"{PROJECT}",
-    workflow=f"{WORKFLOW}",
-    env_vars=update_env_vars(base_env_vars, {"STEP": "create_curated_database"})
-)
-tasks["raw_hist_to_curated"] >> tasks["create_curated_database"]
+    tasks[f"raw_hist_to_curated_close_{table}"] = AnalyticalPlatformStandardOperator(
+        dag=dag,
+        task_id=f"raw_hist_to_curated_close_{table}",
+        name=f"{PROJECT}.{WORKFLOW}",
+        compute_profile="general-spot-1vcpu-4gb",
+        image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
+        environment=f"{ENVIRONMENT}",
+        project=f"{PROJECT}",
+        workflow=f"{WORKFLOW}",
+        env_vars=update_env_vars(base_env_vars, {"STEP": "raw_hist_to_curated", "TOTAL_WORKERS": total_workers, "CLOSE": True, "TABLE": table})
+    )
+
+    for batch in range (total_workers):
+        tasks[f"raw_hist_to_curated_{table}_{batch}"] = AnalyticalPlatformStandardOperator(
+            dag=dag,
+            task_id=f"raw_hist_to_curated_{table}_{batch}",
+            name=f"{PROJECT}.{WORKFLOW}",
+            compute_profile="general-spot-1vcpu-4gb",
+            image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
+            environment=f"{ENVIRONMENT}",
+            project=f"{PROJECT}",
+            workflow=f"{WORKFLOW}",
+            env_vars=update_env_vars(base_env_vars, {"STEP": "raw_hist_to_curated", "TOTAL_WORKERS": total_workers, "CLOSE": False, "CURRENT_WORKER": batch, "TABLE": table})
+        )
+        tasks[f"raw_hist_to_curated_init_{table}"] >> tasks[f"raw_hist_to_curated_{table}_{batch}"]
+        tasks[f"raw_hist_to_curated_{table}_{batch}"] >> tasks[f"raw_hist_to_curated_close_{table}"]
+        tasks[f"raw_hist_to_curated_close_{table}"] >> tasks[f"create_curated_database"]
