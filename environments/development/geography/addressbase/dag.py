17918c1d580f2e44c33fcf6cd100ba9aa28965b0
Philip <112626966+p-sin@users.noreply.github.com>
2025-10-22 12:10:25 +0100

    Geog: Addressbase - rename step (#1818)

--- environments/development/geography/addressbase/dag.py
+++ environments/development/geography/addressbase/dag.py
@@ -92,20 +92,20 @@ for table in raw_tables:
         environment=f"{ENVIRONMENT}",
         project=f"{PROJECT}",
         workflow=f"{WORKFLOW}",
-        env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw", "TOTAL_WORKERS": total_workers, "CLOSE": False, "TABLE": table})
+        env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw_hist", "TOTAL_WORKERS": total_workers, "CLOSE": False, "TABLE": table})
     )
     tasks["to_land"] >> tasks[f"land_to_raw_hist_init_{table}"]
 
-    tasks[f"land_to_raw_close_{table}"] = AnalyticalPlatformStandardOperator(
+    tasks[f"land_to_raw_hist_close_{table}"] = AnalyticalPlatformStandardOperator(
         dag=dag,
-        task_id=f"land_to_raw_close_{table}",
+        task_id=f"land_to_raw_hist_close_{table}",
         name=f"{PROJECT}.{WORKFLOW}",
         compute_profile="general-spot-1vcpu-4gb",
         image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
         environment=f"{ENVIRONMENT}",
         project=f"{PROJECT}",
         workflow=f"{WORKFLOW}",
-        env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw", "TOTAL_WORKERS": total_workers, "CLOSE": True, "TABLE": table})
+        env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw_hist", "TOTAL_WORKERS": total_workers, "CLOSE": True, "TABLE": table})
     )
 
     tasks[f"raw_hist_to_curated_init_{table}"] = AnalyticalPlatformStandardOperator(
@@ -119,22 +119,22 @@ for table in raw_tables:
         workflow=f"{WORKFLOW}",
         env_vars=update_env_vars(base_env_vars, {"STEP": "raw_hist_to_curated", "TOTAL_WORKERS": total_workers, "CLOSE": False, "TABLE": table})
     )
-    tasks[f"land_to_raw_close_{table}"] >> tasks[f"raw_hist_to_curated_init_{table}"]
+    tasks[f"land_to_raw_hist_close_{table}"] >> tasks[f"raw_hist_to_curated_init_{table}"]
 
     for batch in range (total_workers):
-        tasks[f"land_to_raw_{table}_{batch}"] = AnalyticalPlatformStandardOperator(
+        tasks[f"land_to_raw_hist_{table}_{batch}"] = AnalyticalPlatformStandardOperator(
             dag=dag,
-            task_id=f"land_to_raw_{table}_{batch}",
+            task_id=f"land_to_raw_hist_{table}_{batch}",
             name=f"{PROJECT}.{WORKFLOW}",
             compute_profile="general-spot-1vcpu-4gb",
             image=f"509399598587.dkr.ecr.eu-west-2.amazonaws.com/{REPOSITORY_NAME}:{REPOSITORY_TAG}",
             environment=f"{ENVIRONMENT}",
             project=f"{PROJECT}",
             workflow=f"{WORKFLOW}",
-            env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw", "TOTAL_WORKERS": total_workers, "CLOSE": False, "CURRENT_WORKER": batch, "TABLE": table})
+            env_vars=update_env_vars(base_env_vars, {"STEP": "land_to_raw_hist", "TOTAL_WORKERS": total_workers, "CLOSE": False, "CURRENT_WORKER": batch, "TABLE": table})
         )
-        tasks[f"land_to_raw_hist_init_{table}"] >> tasks[f"land_to_raw_{table}_{batch}"]
-        tasks[f"land_to_raw_{table}_{batch}"] >> tasks[f"land_to_raw_close_{table}"]
+        tasks[f"land_to_raw_hist_init_{table}"] >> tasks[f"land_to_raw_hist_{table}_{batch}"]
+        tasks[f"land_to_raw_hist_{table}_{batch}"] >> tasks[f"land_to_raw_hist_close_{table}"]
 
 
     tasks[f"raw_hist_to_curated_close_{table}"] = AnalyticalPlatformStandardOperator(
