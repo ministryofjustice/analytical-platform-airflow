a88e7b93c0194e265949bc57988e700f9b00bf23
Anthony Cody <anthony.cody@digital.justice.gov.uk>
2025-12-09 12:21:36 +0000

    Update dag.py

--- environments/production/nomis/nomis-extraction/dag.py
+++ environments/production/nomis/nomis-extraction/dag.py
@@ -247,10 +247,7 @@ tasks["main"] = AnalyticalPlatformStandardOperator(
 # )
 
 # # Deletes
-for i, L in PK_EXTRACTIONS.items():
-    if i in PK_EXCEPTIONS and datetime.now().day not in PK_EXCEPTIONS[i]:
-        continue
-    tables_string = ",".join(L)
+
 # tasks[f"nomis-pk-deletes-extracts-{i}"] = AnalyticalPlatformStandardOperator(
 #     dag=dag,
 #     task_id=f"nomis-pk-deletes-extracts-{i}",
