6b9e16b40fe405c7b6764c9686682c475baaadc7
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2026-03-16 16:13:37 +0000

    sparated into prod and non-prod deployments

--- terraform/scripts/eks-authentication.sh
+++ terraform/scripts/eks-authentication.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-
 EKS_CLUSTER_NAME="${1}"
 
-aws eks get-token --cluster-name "${EKS_CLUSTER_NAME}"
+unset $(env | awk -F= '/^AWS_/ {print $1}')
+AWS_SSO_FILE_PASSWORD=a7b3104e753a0d5e70ed2548dfe78dcc280605fd8bdef638ac6694e10c891238
+aws-sso login
+aws-sso exec --profile $EKS_CLUSTER_NAME:platform-engineer-admin -- aws eks get-token --cluster-name "${EKS_CLUSTER_NAME}"
