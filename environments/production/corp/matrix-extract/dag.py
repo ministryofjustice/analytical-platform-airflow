b7c503cf7c4bc5d7f6389b4cddd1e9b1297ca6e3
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-09-08 15:58:16 +0100

    added secret import to dag

--- environments/production/corp/matrix-extract/dag.py
+++ environments/production/corp/matrix-extract/dag.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow.models import DAG
 from analytical_platform.standard_operator import AnalyticalPlatformStandardOperator
+from analytical_platform.secret import Secret
 
 
 REPOSITORY_NAME = "PLACEHOLDER_REPOSITORY_NAME"
@@ -49,6 +50,14 @@ def create_kubernetes_operator(task_id, run_function, dag, scrape_date=DATE):
         environment=f"{ENVIRONMENT}",
         project=f"{PROJECT}",
         workflow=f"{WORKFLOW}",
+        secrets=[
+            Secret(
+                deploy_type="env",
+                deploy_target='SECRET_MATRIX_KEY',
+                secret=f"{PROJECT}-{WORKFLOW}-matrix-key",
+                key="data",
+            )
+        ],        
         env_vars={
             "AWS_METADATA_SERVICE_TIMEOUT": "60",
             "AWS_METADATA_SERVICE_NUM_ATTEMPTS": "5",
